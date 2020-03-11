a6ee7ab1f5e9305a666d6a3d61af51c244648146
joshrenema2 <josh.renema@icloud.com>
2020-03-11 15:44:39 -0700

    added html

---
+++ dynamic-midterm/deductive_content.js
@@ -0,0 +1,29 @@
+var data = {
+    qnum:0,
+    questions:[
+        {
+            title: "What comes after A?",
+            answer: "B",
+            bgcolor: "yellow",
+            img: "http://placekitten.com/100/100",
+        },
+        {
+            title: "Is 5 bigger than 4?",
+            answer: "Yes",
+            bgcolor: "#ABC",
+            img: "http://placekitten.com/200/200",
+        },
+        {
+            title: "What is the color of a banana?",
+            answer: "Yellow",
+            bgcolor: "rgb(1,150,232)",
+            img: "http://placekitten.com/300/300",
+        },
+        {
+            title: "What is the meaning of life?",
+            answer: "idk",
+            bgcolor: "aquamarine",
+            img: "http://placekitten.com/400/400",
+        }
+    ]
+}
