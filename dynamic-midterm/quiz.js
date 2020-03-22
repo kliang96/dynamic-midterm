cb5c9ea56bea315f1860fdaf176e70da72f8e2d9
kliang96 <570789@gmail.com>
2020-03-22 00:22:50 -0700

    done!

--- dynamic-midterm/quiz.js
+++ dynamic-midterm/quiz.js
@@ -1,5 +1,7 @@
 var data = {
     number: 0,
+    points: 0,
+    wrong: "Nice try, right now you are at:",
     question1: [{
             questionNo: "Question 1",
             question: "I knocked down a player in Fortnite",
@@ -128,13 +130,11 @@ function next1() {
 }
 
 var counter = 0;
-var aswr = 1;
 
 function next() {
     // console.log(data.number, "data.number");
     if (counter <= 2) {
         question1();
-        aswr = 1;
     }
     if (counter >= 3 && counter <= 5) {
         if (data.number == 3) {
@@ -143,7 +143,6 @@ function next() {
             document.querySelector(".quiz_answer").style.display = "flex";
         }
         question2();
-        aswr = 2;
     }
     if (counter >= 6 && counter <= 8) {
         if (data.number == 3) {
@@ -152,7 +151,6 @@ function next() {
             document.querySelector(".quiz_answer").style.display = "flex";
         }
         question3();
-        aswr = 3;
     }
     if (counter >= 9 && counter <= 11) {
         if (data.number == 3) {
@@ -161,7 +159,6 @@ function next() {
             document.querySelector(".quiz_answer").style.display = "flex";
         }
         question4();
-        aswr = 4;
     }
     if (counter >= 12 && counter <= 14) {
         if (data.number == 3) {
@@ -170,17 +167,19 @@ function next() {
             document.querySelector(".quiz_answer").style.display = "flex";
         }
         question5();
-        aswr = 5;
 
     }
-    if (counter == 15 ) {
+    if (counter >= 15 && counter < 16) {
+        data.number = 0;
         document.querySelector(".quiz_question").style.display = "none";
         document.querySelector(".quiz_answer").style.display = "flex";
     }
-    if (counter >= 16){
-        document.querySelector(".quiz_question").style.display = "none";
+    if (counter >= 16) {
         document.querySelector(".right").style.display = "flex";
+        document.querySelector(".quiz_answer").style.display = "none";
+        document.querySelector(".quiz_question").style.display = "none";
         document.querySelector("#done").style.display = "none";
+        document.querySelector(".point").innerHTML = data.points;
     }
     console.log(counter, "counter");
     counter++;
@@ -230,48 +229,54 @@ function question5() {
     // data.number++;
 }
 
-var points = 0;
+// var points = 0;
 
 function answer(clicked) {
     if (clicked == data.question1[data.number].answer && counter === 4) {
         document.querySelector(".right").style.display = "flex";
         document.querySelector(".quiz_answer").style.display = "none";
         document.querySelector(".quiz_question").style.display = "none";
-        points++;
-        document.querySelector(".mark").innerHTML = points;
+        data.points++;
+        document.querySelector(".mark").innerHTML = data.points;
         console.log("one");
     } else if (clicked == data.question2[data.number].answer && counter === 7) {
         document.querySelector(".right").style.display = "flex";
         document.querySelector(".quiz_answer").style.display = "none";
         document.querySelector(".quiz_question").style.display = "none";
-        points++;
-        document.querySelector(".mark").innerHTML = points;
+        data.points++;
+        document.querySelector(".mark").innerHTML = data.points;
         console.log("two");
     } else if (clicked == data.question3[data.number].answer && counter == 10) {
         document.querySelector(".right").style.display = "flex";
         document.querySelector(".quiz_answer").style.display = "none";
         document.querySelector(".quiz_question").style.display = "none";
-        points++;
-        document.querySelector("#mark").innerHTML = points;
+        data.points++;
+        document.querySelector("#mark").innerHTML = data.points;
     } else if (clicked == data.question4[data.number].answer && counter == 13) {
         document.querySelector(".right").style.display = "flex";
         document.querySelector(".quiz_answer").style.display = "none";
         document.querySelector(".quiz_question").style.display = "none";
-        points++;
-        document.querySelector(".mark").innerHTML = points;
+        data.points++;
+        document.querySelector(".mark").innerHTML = data.points;
     } else if (clicked == data.question5[data.number].answer && counter == 16) {
-        document.querySelector(".right").style.display = "flex";
+        data.points++;
+        document.querySelector(".score").innerHTML = "Thank you for playing! Your final score is " + data.points + "/5! Good job";
         document.querySelector(".quiz_answer").style.display = "none";
         document.querySelector(".quiz_question").style.display = "none";
-        points++;
-        document.querySelector(".mark").innerHTML = points;
+        document.querySelector(".right").style.display = "flex";
+    // } else if (clicked != data.question5[data.number].answer && counter >= 16) {
+    //     document.querySelector(".right").style.display = "flex";
+    //     document.querySelector(".quiz_answer").style.display = "none";
+    //     document.querySelector(".quiz_question").style.display = "none";
+    //     document.querySelector(".mark").innerHTML = data.points;
     } else {
+        document.querySelector(".scores").innerHTML = "Nice try, you are now at " + data.points + "/5 Keep it up!";
         document.querySelector(".wrong").style.display = "flex";
         document.querySelector(".quiz_answer").style.display = "none";
         document.querySelector(".quiz_question").style.display = "none";
-        document.querySelector(".mark").innerHTML = points;
+        console.log("wrong");
     }
-    console.log(points, "points");
+    console.log(data.points, "points");
 }
 
 function changedisp() {
